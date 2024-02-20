7c649fa8ee50c7b6e51d092e528a009cb87f82cb
abdellatif <abdellatif@debian>
2024-02-20 22:36:48 +0100

    parse input for cheapest_products()

--- submissions/AbdellatifTensai/main.c
+++ submissions/AbdellatifTensai/main.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
-#define INPUT_COUNT 1024
-static char input[INPUT_COUNT];
+#define MAX_INPUT_COUNT 1024
+static char input[MAX_INPUT_COUNT];
+static int inputCount;
 
 void slurp_file(){
-	FILE *file = fopen("input.txt", "rb");
+	FILE *file = fopen("test.txt", "rb");
 	assert(file);
-	fread(input, sizeof(*input), INPUT_COUNT, file);
+	fread(input, sizeof(*input), MAX_INPUT_COUNT, file);
+	inputCount = ftell(file);
 	fclose(file);
+}
+
+void cheapest_products(){
+	char *currentChar = input;
+
+	while((currentChar - input) < inputCount){
+		while(*currentChar++ != ',');
+
+		char *currentProduct = currentChar;
+		while(*currentChar++ != ',');
+		int currentProductLength = currentChar - currentProduct - 1;
+
+		char *currentPrice = currentChar;
+		while(*currentChar++ != '\n');
+		int currentPriceLength = currentChar - currentPrice - 1;
+
+		write(1, currentProduct, currentProductLength);
+		write(1, " : ", 3);
+		write(1, currentPrice, currentPriceLength);
+		write(1, "\n", 1);
+	}
+	fflush(stdout);
 }
 
 int main(){
 	slurp_file();
-	printf("%s", input);
+	cheapest_products();
+	printf("\n%s", input);
+
 	return 0;
 }
